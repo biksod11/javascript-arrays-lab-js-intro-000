cd792dd0e07f85fc740783147cfc00b8dc26640e
Bikram Sodhi <biksodhi@gmail.com>
2020-01-22 20:41:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   return kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift(name)
+}
